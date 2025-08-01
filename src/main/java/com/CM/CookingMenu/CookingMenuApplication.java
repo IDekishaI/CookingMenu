b67401bfcf67598d60cf37c9c2738d7cf3dc4563
Deki <110354843+IDekishaI@users.noreply.github.com>
2025-08-01 18:56:32 +0200

    Add files via upload

---
+++ src/main/java/com/CM/CookingMenu/CookingMenuApplication.java
@@ -0,0 +1,13 @@
+package com.CM.CookingMenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CM/CookingMenu/CookingMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CM.CookingMenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
